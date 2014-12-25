f4dc66baa2928f526af868506c7f93f8f543eca3
Paul Milla <paul.milla1@utexas.edu>
2014-12-24 19:23:18 -0600

    Created an Intent to start another activity

--- app/src/main/java/com/test/paulm/tutorial/MainActivity.java
+++ app/src/main/java/com/test/paulm/tutorial/MainActivity.java
@@ -1,12 +1,16 @@
 package com.test.paulm.tutorial;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "com.test.paulm.tutorial.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +39,18 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Does something when the send button is clicked
+     * @param view  The View button the user clicked
+     */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
